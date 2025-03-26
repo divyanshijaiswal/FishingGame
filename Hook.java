e50c8c89545ececd38a75b9ed832b2e3bc9dea69
divyanshijaiswal <divyanshiJ777@gmail.com>
2025-03-25 20:15:33 -0700

    Add files via upload

---
+++ Hook.java
@@ -0,0 +1,15 @@
+/**
+ * Activity 4.9.3
+ */
+public class Hook extends LakeObject
+{
+  private int strength = 45;
+
+  /*---------- accessor ----------*/
+  public int getStrength() 
+  {
+    return strength; // This return will be updated in the next activity
+    //return strength;
+  }
+
+}
